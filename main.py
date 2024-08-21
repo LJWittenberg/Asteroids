c79966004a31e7341b7ff5ed53782164cfb28279
Lennard Wittenberg <lennwitt@freenet.de>
2024-08-21 10:25:34 +0200

    Add: constants.py; Update: main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
